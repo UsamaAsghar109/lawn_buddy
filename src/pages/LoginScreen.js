0767909dce9d1b2ba425420376f06098aa2d0504
UsamaAsghar109 <usamaasghar109@gmail.com>
2022-05-09 01:19:28 +0500

    first commit

---
+++ src/pages/LoginScreen.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { TextInput, View } from 'react-native';
+
+const LoginScreen = () => {
+    const []
+    return (
+        <View>
+            <Text>LoginScreen</Text>
+            <TextInput placeholder="Type user" />
+            <TextInput placeholder="Typer Password" />
+        </View>
+    )
+}
+
+export default LoginScreen;
